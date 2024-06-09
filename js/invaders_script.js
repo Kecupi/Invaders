f6a922fa4f8a787b9bf904137414761a3c564522
Stepan Horenek <stepan.horenek@gmail.com>
2024-06-09 20:50:31 +0200

    Generalised the width and height of player, boss and enemies

--- js/invaders_script.js
+++ js/invaders_script.js
@@ -11,7 +11,6 @@ startScreen = (text) =>{
     start.style.left = "50%";
     start.style.transform = "translate(-50%,-50%)"
     document.body.insertBefore(start, document.body.childNodes[0]);
-    
     start.addEventListener("click", () => {
         GameArea.start();
         document.getElementById("theme").play();
@@ -31,14 +30,14 @@ var GameArea = {
         this.canvas.width = window.innerWidth;
         this.canvas.height = window.innerHeight;
         this.context = this.canvas.getContext("2d");
-        this.context.font = ((window.innerWidth+window.innerHeight)/60)+"px Arial";
+        this.context.font = ((window.innerWidth+window.innerHeight)/50)+"px Arial";
         this.context.fillStyle = "white";
         document.body.insertBefore(this.canvas, document.body.childNodes[0]);
         this.frameNumber = 0;
         this.bossfight = false;
         this.interval = setInterval(this.updateGameArea, 10);
         this.score = 0;
-        Hrac = new Player (50, 50, "images/player.gif");
+        Hrac = new Player (window.innerWidth*0.1, window.innerHeight/2, "images/player.gif");
         Bos = 0;
         Enemies = [];
         Bullets = [];
@@ -53,9 +52,17 @@ var GameArea = {
         window.addEventListener('resize', () =>{
             this.canvas.width = window.innerWidth;
             this.canvas.height = window.innerHeight;
-            this.context.font = ((window.innerWidth+window.innerHeight)/60)+"px Arial";
+            this.context.font = ((window.innerWidth+window.innerHeight)/50)+"px Arial";
             this.context.fillStyle = "white";
             Bos.x = window.innerWidth*0.9;
+            Bos.height = (window.innerWidth+window.innerHeight)/50;
+            Bos.width = (window.innerWidth+window.innerHeight)/50;
+            Hrac.height = (window.innerWidth+window.innerHeight)/50;
+            Hrac.width = (window.innerWidth+window.innerHeight)/50;
+            for (let i =0 ;i<Enemies.length;i++){
+                Enemies[i].height = (window.innerWidth+window.innerHeight)/50;
+                Enemies[i].width = (window.innerWidth+window.innerHeight)/50;
+            }
             this.clear();
             if (this.interval !=null){
                 this.updateScore()
@@ -77,8 +84,6 @@ var GameArea = {
         document.getElementById("theme").pause();
         document.getElementById("theme").currentTime = 0;
         startScreen("Restart");
-        
-        
     },
     updateScore: function(){
         this.context.fillText(`Score: ${this.score}`,10,50);
@@ -99,7 +104,7 @@ var GameArea = {
                 Bullets[i].crash();
             }
         }
-        if (GameArea.score%5 == 0 && GameArea.score !=0 && GameArea.bossfight == false){
+        if (GameArea.score%50 == 0 && GameArea.score !=0 && GameArea.bossfight == false){
             GameArea.bossfight=true;
             GameArea.clear();
             Bos = new Boss("images/boss.png");
@@ -127,9 +132,11 @@ class Player{
         this.image.src = imgsrc;
         this.bulletRate = 0;
         this.explode = false;
+        this.height = (window.innerWidth+window.innerHeight)/50;
+        this.width = (window.innerWidth+window.innerHeight)/50;
     }
     update(){
-        GameArea.context.drawImage(this.image, this.x, this.y);
+        GameArea.context.drawImage(this.image, this.x, this.y, this.width,this.height);
     }
     changePos(){
         this.bulletRate += 1;
@@ -141,7 +148,7 @@ class Player{
         if (GameArea.keys && GameArea.keys['ArrowDown'] && Hrac.y<(window.innerHeight-70)) {Hrac.speedY = 15;};
         if (this.bulletRate>=20){
             if (GameArea.keys && GameArea.keys[" "]) {
-            Bullets.push(new Bullet(this.x + this.image.width,this.y,10,"images/bullet.png"),new Bullet(this.x + this.image.width,this.y+this.image.height,10,"images/bullet.png"));
+            Bullets.push(new Bullet(this.x + this.width,this.y,10,"images/bullet.png"),new Bullet(this.x + this.width,this.y+this.height,10,"images/bullet.png"));
             this.bulletRate = 0;
             }
         }
@@ -156,12 +163,12 @@ class Player{
     }
     crash(){
         for (let i=0;i<Enemies.length;i++){
-            if ((Enemies[i].x + Enemies[i].image.width >= this.x && Enemies[i].x <= this.x + this.image.width) && ((Enemies[i].y + Enemies[i].image.height >= this.y && Enemies[i].y <= this.y + this.image.height) ||(this.y + this.image.height >= Enemies[i].y && this.y <= Enemies[i].y + Enemies[i].image.height)))
+            if ((Enemies[i].x + Enemies[i].width >= this.x && Enemies[i].x <= this.x + this.width) && ((Enemies[i].y + Enemies[i].height >= this.y && Enemies[i].y <= this.y + this.height) ||(this.y + this.height >= Enemies[i].y && this.y <= Enemies[i].y + Enemies[i].height)))
                 {this.explode = true;}
         }
     }
     crashBoss(){
-        if(this.x <= (Bos.x+Bos.image.width) && this.x >= Bos.x && this.y>=Bos.y && this.y<=(Bos.image.height + Bos.y)){
+        if(this.x <= (Bos.x+Bos.width) && this.x >= Bos.x && this.y>=Bos.y && this.y<=(Bos.height + Bos.y)){
             this.explode = true;
         }
     }
@@ -178,11 +185,13 @@ class Enemy{
         this.speedX = Math.floor(Math.random()*(5)+10);
         this.image = new Image;
         this.image.src = imgsrc;
+        this.height = (window.innerWidth+window.innerHeight)/50;
+        this.width = (window.innerWidth+window.innerHeight)/50;
     }
     update(){
         this.x -= this.speedX;
         this.y -= this.speedY; 
-        GameArea.context.drawImage(this.image, this.x, this.y);
+        GameArea.context.drawImage(this.image, this.x, this.y, this.width,this.height);
     }
 }
 class Boss{
@@ -194,11 +203,13 @@ class Boss{
         this.hp = 2;
         this.up = false;
         this.bulletRate = 0;
+        this.height = (window.innerWidth+window.innerHeight)/50;
+        this.width = (window.innerWidth+window.innerHeight)/50;
     }
     update(){
         this.bulletRate += 1;
         if (this.bulletRate>=20){
-            bossBullets.push(new Bullet(this.x + this.image.width,this.y,-10,"images/bullet.png"));
+            bossBullets.push(new Bullet(this.x,this.y+this.height/2,-10,"images/bullet.png"));
             this.bulletRate = 0;
         }
         for (let i=0;i<bossBullets.length;i++){
@@ -216,7 +227,7 @@ class Boss{
                 this.up = false;
             }
         }
-        GameArea.context.drawImage(this.image, this.x, this.y);
+        GameArea.context.drawImage(this.image, this.x, this.y, this.width,this.height);
         if(this.hp==0){
             GameArea.bossfight=false;
             bossBullets = [];
@@ -239,7 +250,7 @@ class Bullet{
     }
     crash(){
         for (let j=0;j<Enemies.length;j++){
-            if (this.x <= (Enemies[j].x+Enemies[j].image.width) && this.x >= Enemies[j].x && this.y>=Enemies[j].y && this.y<=(Enemies[j].image.height + Enemies[j].y)){
+            if (this.x <= (Enemies[j].x+Enemies[j].width) && this.x >= Enemies[j].x && this.y>=Enemies[j].y && this.y<=(Enemies[j].height + Enemies[j].y)){
                 Bullets.splice(Bullets.indexOf(this),1);
                 Enemies.splice(j,1);
                 document.getElementById("death_sound").play();
@@ -249,13 +260,13 @@ class Bullet{
         }
     }
     crashBoss(){
-        if(this.x <= (Bos.x+Bos.image.width) && this.x >= Bos.x && this.y>=Bos.y && this.y<=(Bos.image.height + Bos.y)){
+        if(this.x <= (Bos.x+Bos.width) && this.x >= Bos.x && this.y>=Bos.y && this.y<=(Bos.height + Bos.y)){
             Bullets.splice(Bullets.indexOf(this),1);
             Bos.hp= Bos.hp-1;
         }
     }
     crashPlayer(){
-        if(this.x <= (Hrac.x+Hrac.image.width) && this.x >= Hrac.x && this.y>=Hrac.y && this.y<=(Hrac.image.height + Hrac.y)){
+        if(this.x <= (Hrac.x+Hrac.image.width) && this.x >= Hrac.x && this.y>=Hrac.y && this.y<=(Hrac.height + Hrac.y)){
             Bullets.splice(Bullets.indexOf(this),1);
             Hrac.explode = true;
         }
